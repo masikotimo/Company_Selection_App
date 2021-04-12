99d24e2c431c56c1084710eab8eb86625cb3ab60
masikotimo <masikotimo@gmail.com>
2021-04-12 20:16:06 +0300

    initial setup of the two screens

--- src/utils/slides.js
+++ src/utils/slides.js
@@ -1,25 +1,25 @@
 const slides = [
-    {
-      key: 1,
-      title: 'Title 1',
-      text: 'Description.\nSay something cool',
-      image: require('./assets/1.jpg'),
-      backgroundColor: '#59b2ab',
-    },
-    {
-      key: 2,
-      title: 'Title 2',
-      text: 'Other cool stuff',
-      image: require('./assets/2.jpg'),
-      backgroundColor: '#febe29',
-    },
-    {
-      key: 3,
-      title: 'Rocket guy',
-      text: 'I\'m already out of descriptions\n\nLorem ipsum bla bla bla',
-      image: require('./assets/3.jpg'),
-      backgroundColor: '#22bcb5',
-    }
-  ];
+  {
+    key: 1,
+    title: 'Title 1',
+    text: 'Description.\nSay something cool',
+    image: '',
+    backgroundColor: '#59b2ab',
+  },
+  {
+    key: 2,
+    title: 'Title 2',
+    text: 'Other cool stuff',
+    image: '',
+    backgroundColor: '#febe29',
+  },
+  {
+    key: 3,
+    title: 'Rocket guy',
+    text: "I'm already out of descriptions\n\nLorem ipsum bla bla bla",
+    image: '',
+    backgroundColor: '#22bcb5',
+  },
+];
 
-  module.exports={slides}
+module.exports = { slides };
